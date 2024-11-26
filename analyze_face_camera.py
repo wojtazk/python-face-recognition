55cc862bfbae6d683d7789ac526304295dcb2f86
Wojciech Kowal <w.kowal202@gmail.com>
2024-11-26 15:02:24 +0100

    Refactor camera face recognition

--- analyze_face_camera.py
+++ analyze_face_camera.py
@@ -1,20 +1,58 @@
-from helpers import opencv_decorator_camera, detect_spoofing, analyze_faces
+import cv2
+
+from helpers import detect_spoofing, analyze_faces
 
 import tensorflow as tf
 # Hide GPU from visible devices
 tf.config.set_visible_devices([], 'GPU')
 
 
-def analyze_face_camera(frame=None) -> None:
-    if frame is None:
-        return
+cv2.startWindowThread()
+cap = cv2.VideoCapture(0)  # 0 - initialize video capture from the default camera
 
-    # detect spoofing, draw rectangle and spoofing info
-    detect_spoofing(frame)
+window_name = 'Hello There'
+cv2.namedWindow(window_name, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
 
-    # analyze faces and draw results
-    analyze_faces(frame)
+while True:
+    ret, frame = cap.read()  # read the frame from the camera
 
+    # frame = cv2.resize(frame, (640, 480))  # resize the frame
+    frame = cv2.flip(frame, 1)  # flip the frame horizontally
 
-if __name__ == '__main__':
-    opencv_decorator_camera(analyze_face_camera)
+    # pad the frame
+    # frame = cv2.copyMakeBorder(frame, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+
+    # show the camera output
+    cv2.imshow(window_name, frame)
+
+    # get pressed key
+    pressed_key = cv2.waitKey(1) & 0xFF
+    # Press 'q' to quit
+    if pressed_key == ord('q'):
+        break
+    # quit when pressing the exit button
+    if cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE) < 1:
+        break
+
+    # Press 'f' to pay respect
+    if pressed_key == ord('f'):
+
+        # detect spoofing, draw rectangle and spoofing info
+        detect_spoofing(frame)
+
+        # add padding to the frame
+        frame = cv2.copyMakeBorder(frame, 200, 200, 200, 200, cv2.BORDER_CONSTANT, value=(0, 0, 0, 0))
+
+        # analyze faces and draw results
+        analyze_faces(frame)
+
+        while True:
+            cv2.imshow(window_name, frame)
+
+            # Press 'q' to stop showing the prediction
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+cap.release()
+cv2.destroyAllWindows()
+
--- helpers.py
+++ helpers.py
@@ -16,56 +16,6 @@ models = [
 ]
 
 
-def opencv_decorator_camera(func):
-    cv2.startWindowThread()
-    cap = cv2.VideoCapture(0)  # 0 - initialize video capture from the default camera
-
-    window_name = 'Hello There'
-    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
-
-    while True:
-        ret, frame = cap.read()  # read the frame from the camera
-
-        # frame = cv2.resize(frame, (640, 480))  # resize the frame
-        frame = cv2.flip(frame, 1)  # flip the frame horizontally
-
-        # pad the frame
-        # frame = cv2.copyMakeBorder(frame, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value=(0, 0, 0))
-
-        # show the camera output
-        cv2.imshow(window_name, frame)
-
-        # get pressed key
-        pressed_key = cv2.waitKey(1) & 0xFF
-        # Press 'q' to quit
-        if pressed_key == ord('q'):
-            break
-        # quit when pressing the exit button
-        if cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE) < 1:
-            break
-
-        # Press 'f' to pay respect
-        if pressed_key == ord('f'):
-
-            func(frame)  # call passed function
-
-            # add padding to the frame
-            frame = cv2.copyMakeBorder(frame, 1000, 1000, 1000, 1000, cv2.BORDER_CONSTANT, value=(0, 0, 0, 0))
-
-            while True:
-                cv2.imshow(window_name, frame)
-
-                # Press 'q' to stop showing the prediction
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-    return func
-
-
-#############################
 def detect_spoofing(frame):
     extracted_faces = DeepFace.extract_faces(
         frame,
