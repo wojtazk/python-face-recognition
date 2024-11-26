39192da83b7192b8bfcf2ace1e940f44d0933b0c
Wojciech Kowal <w.kowal202@gmail.com>
2024-11-26 14:49:52 +0100

    Refactor spoofing detection and face analysis

--- analyze_face_camera.py
+++ analyze_face_camera.py
@@ -1,82 +1,19 @@
-import cv2
-from deepface import DeepFace
-
-from helpers import opencv_decorator_camera, models
+from helpers import opencv_decorator_camera, detect_spoofing, analyze_faces
 
 import tensorflow as tf
 # Hide GPU from visible devices
 tf.config.set_visible_devices([], 'GPU')
 
 
-# loading model into memory
-DeepFace.build_model(models[0])
-
-
 def analyze_face_camera(frame=None) -> None:
     if frame is None:
         return
 
-    extracted_faces = DeepFace.extract_faces(
-        frame,
-        anti_spoofing=True,
-        enforce_detection=False,
-    )
-    for i in range(len(extracted_faces)):
-        print(extracted_faces[i])
+    # detect spoofing, draw rectangle and spoofing info
+    detect_spoofing(frame)
 
-        # face = extracted_faces[i]['face']
-        facial_area = extracted_faces[i]['facial_area']
-        face_confidence = extracted_faces[i]['confidence']
-
-        is_real = extracted_faces[i]['is_real']
-        anti_spoof_score = extracted_faces[i]['antispoof_score']
-
-        # get region with detected face
-        x = facial_area['x']
-        y = facial_area['y']
-        w = facial_area['w']
-        h = facial_area['h']
-
-        # draw rectangle around the face
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 153, 0), 2)
-
-        # display face confidence
-        text = f"Face confidence: {face_confidence * 100:.0f}%"
-        cv2.putText(frame, text, (x + 10, y + h + 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 153, 0), 2)
-
-        # display anti spoof score
-        text = f"Real face: {is_real} ({anti_spoof_score * 100:.2f}%)"
-        cv2.putText(frame, text, (x + 10, y + h + 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 153, 0), 2)
-
-    # analyze face
-    face_analysis = DeepFace.analyze(
-        frame,
-        actions=["emotion", "age", "gender", "race"],
-        enforce_detection=False
-    )
-
-    for i in range(len(face_analysis)):
-        print(face_analysis[i])
-
-        emotion = face_analysis[i]['dominant_emotion']
-        age = int(face_analysis[i]['age'])
-        gender = face_analysis[i]['dominant_gender']
-        race = face_analysis[i]['dominant_race']
-
-        facial_area = face_analysis[i]['region']
-
-        # get region with detected face
-        x = facial_area['x']
-        y = facial_area['y']
-        # w = facial_area['w']
-        h = facial_area['h']
-
-        # # display analysis info
-        texts = f"Emotion: {emotion}\nAge: {age}\nGender: {gender}\nRace: {race}".split('\n')
-        j = 0
-        for text in texts:
-            cv2.putText(frame, text, (x + 10, y + h + 90 + 30 * j), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 153, 0), 2)
-            j += 1
+    # analyze faces and draw results
+    analyze_faces(frame)
 
 
 if __name__ == '__main__':
--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import cv2
+from deepface import DeepFace
 
 
 models = [
@@ -20,6 +21,7 @@ def opencv_decorator_camera(func):
     cap = cv2.VideoCapture(0)  # 0 - initialize video capture from the default camera
 
     window_name = 'Hello There'
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
 
     while True:
         ret, frame = cap.read()  # read the frame from the camera
@@ -28,7 +30,7 @@ def opencv_decorator_camera(func):
         frame = cv2.flip(frame, 1)  # flip the frame horizontally
 
         # pad the frame
-        frame = cv2.copyMakeBorder(frame, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+        # frame = cv2.copyMakeBorder(frame, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value=(0, 0, 0))
 
         # show the camera output
         cv2.imshow(window_name, frame)
@@ -47,6 +49,9 @@ def opencv_decorator_camera(func):
 
             func(frame)  # call passed function
 
+            # add padding to the frame
+            frame = cv2.copyMakeBorder(frame, 1000, 1000, 1000, 1000, cv2.BORDER_CONSTANT, value=(0, 0, 0, 0))
+
             while True:
                 cv2.imshow(window_name, frame)
 
@@ -58,3 +63,82 @@ def opencv_decorator_camera(func):
     cv2.destroyAllWindows()
 
     return func
+
+
+#############################
+def detect_spoofing(frame):
+    extracted_faces = DeepFace.extract_faces(
+        frame,
+        anti_spoofing=True,
+        enforce_detection=False,
+    )
+
+    for i in range(len(extracted_faces)):
+        print(extracted_faces[i])
+
+        # face = extracted_faces[i]['face']
+        facial_area = extracted_faces[i]['facial_area']
+        face_confidence = extracted_faces[i]['confidence']
+
+        is_real = extracted_faces[i]['is_real']
+        anti_spoof_score = extracted_faces[i]['antispoof_score']
+
+        # get region with detected face
+        x = facial_area['x']
+        y = facial_area['y']
+        w = facial_area['w']
+        h = facial_area['h']
+
+        # draw rectangle around the face
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 153, 0), 2)
+
+        # display face confidence
+        text = f"Face confidence: {face_confidence * 100:.0f}%"
+        text_x = x + 10
+        text_y = y + h + 30
+        cv2.putText(frame, text, (text_x + 2, text_y + 2), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
+        cv2.putText(frame, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 153, 0), 2)
+
+        # display anti spoof score
+        text = f"Real face: {is_real} ({anti_spoof_score * 100:.2f}%)"
+        text_y = y + h + 60
+        cv2.putText(frame, text, (text_x + 2, text_y + 2), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
+        cv2.putText(frame, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 153, 0), 2)
+
+
+def analyze_faces(frame):
+    face_analysis = DeepFace.analyze(
+        frame,
+        actions=["emotion", "age", "gender", "race"],
+        enforce_detection=False
+    )
+
+    for i in range(len(face_analysis)):
+        print(face_analysis[i])
+
+        emotion = face_analysis[i]['dominant_emotion']
+        age = int(face_analysis[i]['age'])
+        gender = face_analysis[i]['dominant_gender']
+        race = face_analysis[i]['dominant_race']
+
+        facial_area = face_analysis[i]['region']
+
+        # get region with detected face
+        x = facial_area['x']
+        y = facial_area['y']
+        # w = facial_area['w']
+        h = facial_area['h']
+
+        # # display analysis info
+        texts = f"Emotion: {emotion}\nAge: {age}\nGender: {gender}\nRace: {race}".split('\n')
+        j = 0
+        for text in texts:
+            text_x = x + 10
+            text_y = y + h + 90 + 30 * j
+
+            # shadow
+            cv2.putText(frame, text, (text_x + 2, text_y + 2), cv2.FONT_HERSHEY_SIMPLEX, .8, (0, 0, 0), 2)
+            # text
+            cv2.putText(frame, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, .8, (255, 153, 0), 2)
+
+            j += 1
