c37945c0d73446b87dc3c40db105f0a230b312a5
Wojciech Kowal <w.kowal202@gmail.com>
2024-12-01 21:20:02 +0100

    Update face_analyze_img.py

--- face_analyze_img.py
+++ face_analyze_img.py
@@ -1,5 +1,4 @@
 import cv2
-import uuid
 import os
 
 from helpers import detect_spoofing, analyze_faces, draw_spoofing, draw_face_analysis
@@ -32,7 +31,7 @@ if __name__ == '__main__':
 
         # define window
         cv2.startWindowThread()
-        window_id = str(uuid.uuid4())
+        window_id = str(image)
 
         cv2.namedWindow(window_id, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
         cv2.resizeWindow(window_id, 600, 600)
@@ -43,13 +42,16 @@ if __name__ == '__main__':
         # draw spoofing info and face analysis
         draw_spoofing(frame, spoofing_analysis, border)
         draw_face_analysis(frame, face_analysis, border)
-        while True:
-            cv2.imshow(window_id, frame)
-            # get pressed key
-            pressed_key = cv2.waitKey(1) & 0xFF
-            # Press 'q' to quit
-            if pressed_key == ord('q'):
-                break
-            # quit when pressing the exit button
-            if cv2.getWindowProperty(window_id, cv2.WND_PROP_VISIBLE) < 1:
-                break
+        cv2.imshow(window_id, frame)
+
+    while True:
+        # get pressed key
+        pressed_key = cv2.waitKey(1) & 0xFF
+        # Press 'q' to quit
+        if pressed_key == ord('q'):
+            break
+        # quit when pressing the exit button
+        # if cv2.getWindowProperty(window_id, cv2.WND_PROP_VISIBLE) < 1:
+        #     break
+
+    cv2.destroyAllWindows()
